d583505a673c2ca27e7a97602ce15f8a3f0af498
Cj72001 <00038619@uca.edu.sv>
2022-10-21 23:09:23 -0600

    Add: test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
 import { describe } from 'vitest'
+import {render, screen} from '@testing-library/react'
+import App from './App';
 
 describe('App', () => { 
-  it.todo('should render');
-  it.todo('should have title "Mi DUI es valido?"');
-  it.todo('should have an input text');
-  it.todo('should have a button with text "Validar"');
-  /* Lo que demas que se les ocurra que puedan probar */
+  it("should render", ()=>{
+    render(<App />)
+  })
+
+  it("should have title Mi DUI es valido?", ()=>{
+    render(<App />)
+    expect(screen.getByRole('heading', {name: /mi dui es valido\?/i})).toBeInTheDocument()
+  })
+
+  it("should have an input text", ()=>{
+    render(<App />)
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+
+  it("must have an specific input text for DUI", ()=>{
+    render(<App />)
+    expect(screen.getByPlaceholderText("000000000")).toBeInTheDocument()
+  })
+
+  it("should have an input text", ()=>{
+    render(<App />)
+    expect(screen.getByRole('button', {name: /validar/i})).toBeInTheDocument()
+  })
+
 })
